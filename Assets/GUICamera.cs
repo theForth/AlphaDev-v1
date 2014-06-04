b971e147f66a1ebb75df16e3454576500e53ba1c
Reuben Pereira <reub.pereira@mail.utoronto.ca>
2014-06-03 21:21:28 -0700

    NGUI -latest

---
+++ Assets/GUICamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUICamera : MonoBehaviour
+{
+		public float targetOffsetX = 0;
+		public float targetOffsetY = 0.05f;
+		public float targetOffsetZ = 0.5f;
+		
+		// Use this for initialization
+		public Transform target;
+		void Start ()
+		{
+				target = transform.parent.gameObject.transform;
+				transform.position = target.position + new Vector3 (targetOffsetX, targetOffsetY, targetOffsetZ);
+				transform.Rotate (0, 180, 0);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+		
+		}
+}
